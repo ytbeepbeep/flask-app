306535b8d3038c0a39158ee52cb25db5552fe456
bongi23 <andrea.bongi@live.it>
2018-11-28 18:59:25 +0100

    correct lambda, now home_page shows runs

--- flaskapp/views/home.py
+++ flaskapp/views/home.py
@@ -17,19 +17,19 @@ def index():
     if hasattr(current_user, 'is_authenticated') and current_user.is_authenticated:
         try:
             print("Try to get user data", current_user)
-            reply = DataService().get("/user/%s"%current_user.dataservice_user_id, params={})
+            reply = DataService().get("/users/%s" % current_user.dataservice_user_id, params={})
 
             user = reply.json()
 
-            reply = DataService().get("/runs", params={ 'user_id': current_user.dataservice_user_id })
+            reply = DataService().get("/runs", params={'user_id': current_user.dataservice_user_id})
             if reply is not None:
                 runs = reply.json()
-
                 if len(runs) > 0:
-                    total_average_speed = functools.reduce(lambda x,y: x + y, runs, 0) / len(runs)
+                    total_average_speed = functools.reduce(lambda x, y: x + y, [run['average_speed'] for run in runs], 0) / len(runs)
 
         except Exception as ex:
             print("ERROR: ", ex)
             # TODO: Add an error message
 
-    return render_template("index.html", credential=current_user, user=user, strava_auth_url=strava_auth_url(), total_average_speed=total_average_speed)
+    return render_template("index.html", credential=current_user, user=user, strava_auth_url=strava_auth_url(),
+                           total_average_speed=total_average_speed, runs=runs)
