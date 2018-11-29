232f51efc2fbebe08d74854fbb469b2e1f4949cf
a.liut <andrea.liut@gmail.com>
2018-11-29 01:00:32 +0100

    Tests for statistics page

--- flaskapp/views/statistics.py
+++ flaskapp/views/statistics.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, json, flash
+from flask import Blueprint, render_template, json, flash, make_response
 from flask_login import login_required
 
 from flaskapp.database import db
@@ -14,19 +14,24 @@ statistics = Blueprint('statistics', __name__)
 def get_statistics():
     """Inside the template we retrieve data from run/statistics
     using javascript"""
+    status_code = 200
     runs = []
     stats = dict()
     try:
         reply = DataService().get("/runs", params={'user_id': current_user.dataservice_user_id})
+
+        if reply.status_code is not 200:
+            status_code = reply.status_code
+            raise "error"
+
         runs = reply.json()
 
         for run in runs:
-            print(run)
             stats[run['id']] = [run['average_speed'], run['distance'], run['elapsed_time']]
 
-    except Exception as ex:
-        print(ex)
+    except Exception:
         flash('Cannot get runs data', category='error')
+        status_code = 500
 
-    return render_template("statistics.html", runs=runs, stats=json.dumps(stats))
+    return make_response(render_template("statistics.html", runs=runs, stats=json.dumps(stats)), status_code)
 
